ae892571c52d258d6ef3fe9d6ca4f2ead2e90769
fabricekx <fabricekx@gmail.com>
2025-04-24 10:45:37 +0200

    ajout de COR dans main.ts pour faire fonctionner le front en local

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // autorise ton frontend Vite
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
