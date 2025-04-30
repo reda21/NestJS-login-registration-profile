852515b4b9d91c76da888da20719ac49b179b06f
fabricekx <fabricekx@gmail.com>
2025-04-30 10:13:05 +0200

    ajout d'une route de test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './DTO/register.dto';
 import { LoginDto } from './DTO/login.dto';
+import { PrismaService } from 'src/prisma.service';
+
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly prisma: PrismaService) {}
 
   @Post('register')  // ../auth/register
   register(@Body() dto: RegisterDto) {
@@ -15,5 +17,16 @@ export class AuthController {
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto.email, dto.password);
+  }
+
+  @Get("db-test")
+  async testDb() {
+    try {
+      const users = await this.prisma.user.findMany(); // ou une autre table existante
+      return users;
+    } catch (error) {
+      console.error('Database test error:', error);
+      return { error: error.message };
+    }
   }
 }
