7c270229b93ed174de0df8b75ff1621919e7aaf6
fabricekx <fabricekx@gmail.com>
2025-05-06 13:47:51 +0200

    pas de / à la fin de l'url du front...

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: 'http://localhost:5173', // autorise ton frontend Vite
-    origin: "https://front-register-login-6prtjib7f-fabrices-projects-43ae4b68.vercel.app/", // autorise le front deployé sur vercel
+    origin: "https://front-register-login-6prtjib7f-fabrices-projects-43ae4b68.vercel.app", // autorise le front deployé sur vercel
     credentials: true,
   });
   console.log("database url: ",process.env.DATABASE_URL);
