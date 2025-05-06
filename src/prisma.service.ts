3af454930dfcdc23df0dd6c6e1fc3eab3a5ee561
fabricekx <fabricekx@gmail.com>
2025-05-06 14:29:11 +0200

    ...

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -6,6 +6,8 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
+    console.log("✅ PrismaClient importé !");
+
   }
 
   async onModuleDestroy() {
