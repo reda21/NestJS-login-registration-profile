e7af874ebdd9488d4a4bbb991777f660c1533f42
fabricekx <fabricekx@gmail.com>
2025-05-06 15:24:39 +0200

    ee

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: 'http://localhost:5173', // autorise ton frontend Vite
-    origin: 'https://front-register-login.vercel.app/',
+    origin: 'https://front-register-login.vercel.app/', // front end déployé sur vercel
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
   });
   console.log("database url: ",process.env.DATABASE_URL);
