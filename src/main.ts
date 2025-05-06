000fe601e247978924f23f1d7e5f98cd0677321a
fabricekx <fabricekx@gmail.com>
2025-05-06 13:38:11 +0200

    ajout de mon front dans enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // autorise ton frontend Vite
+    // origin: 'http://localhost:5173', // autorise ton frontend Vite
+    origin: "https://front-register-login-6prtjib7f-fabrices-projects-43ae4b68.vercel.app/", // autorise le front deployé sur vercel
     credentials: true,
   });
   console.log("database url: ",process.env.DATABASE_URL);
